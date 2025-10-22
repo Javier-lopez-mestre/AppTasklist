2afbcdcfd628c1faa83c6dcc36c5844b94ba48e1
javier-lopez-mestre <xalomes276@gmail.com>
2025-10-22 20:49:38 +0200

    tasca v0.2 realitzada

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,7 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
+let $liActual = null; 
+
 function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
 
@@ -10,7 +12,7 @@ function onDeviceReady() {
             "Afegir": function() {
                 let tasca = $("#novaTasca").val().trim();
                 if(tasca) {
-                    $("#tasques").append($("<li>").text(tasca));
+                    creaTasca(tasca);
                     $("#novaTasca").val("");
                     $(this).dialog("close");
                 }
@@ -24,4 +26,63 @@ function onDeviceReady() {
     $("#afegirTasca").click(function() {
         $("#modalTasca").dialog("open");
     });
+
+    $("#modalEdita").dialog({
+        autoOpen: false,
+        modal: true,
+        buttons: {
+            "OK": function() {
+                let nouText = $("#textEdita").val().trim();
+                if(nouText && $liActual) {
+                    $liActual.contents().filter(function() {
+                        return this.nodeType === 3; 
+                    }).first().replaceWith(nouText + " ");
+                }
+                $(this).dialog("close");
+            },
+            "Cancel·lar": function() {
+                $(this).dialog("close");
+            }
+        }
+    });
+
+    let tasquesInicials = ["Tasca 1", "Tasca 2", "Tasca 3"];
+    $("#tasques").empty(); 
+
+    tasquesInicials.forEach(text => {
+        creaTasca(text);
+    });
+}
+
+function creaTasca(text) {
+    let $li = $("<li>").text(text + " ");
+
+    let $btnEditar = $("<button>")
+        .text("Edita")
+        .addClass("boto_edita")
+        .click(editar);
+
+    let $btnEliminar = $("<button>")
+        .text("Elimina")
+        .addClass("boto_eliminar")
+        .click(elimina);
+
+    $li.append($btnEditar).append(" ").append($btnEliminar);
+    $("#tasques").append($li);
+}
+
+function elimina(event) {
+    let caller = event.target || event.srcElement;
+    $(caller).closest("li").remove();
+}
+
+function editar(event) {
+    $liActual = $(event.target).closest("li");
+
+    let text = $liActual.contents().filter(function() {
+        return this.nodeType === 3;
+    }).text().trim();
+
+    $("#textEdita").val(text);
+    $("#modalEdita").dialog("open");
 }
